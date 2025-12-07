b847c2b7b3f62832c289238083f075578625dd8b
GensisEsc <genesisgeo171@correo.com>
2025-12-06 20:02:57 -0500

    Comentarios en HospitalException

--- src/main/java/Util/HospitalException.java
+++ src/main/java/Util/HospitalException.java
@@ -1,9 +1,21 @@
 package Util;
-
+/*
+ * Autor: Génesis Escobar
+ * Fecha: 06-12-2025
+ * Versión: 1.0
+ * Descripción:
+ * Excepción personalizada utilizada dentro del proyecto para representar errores
+ * propios de la lógica del sistema hospitalario. Extiende RuntimeException para
+ * permitir que se lance sin necesidad de declararla explícitamente.
+ *
+ * Se utiliza principalmente en la capa Service para reportar fallos de validaciones,
+ * reglas de negocio o problemas al interactuar con la capa de datos. Facilita la
+ * lectura del código y permite brindar mensajes más claros hacia la API.
+ */
 public class HospitalException extends RuntimeException {
     /**
      * implementamos un constructor donde recibe como parametro un mensaje
-     * luego llamamos a la calase constructor de la clase padre para que
+     * luego llamamos a la clase constructor de la clase padre para que
      * lance la exception
      */
     public HospitalException(String message){
